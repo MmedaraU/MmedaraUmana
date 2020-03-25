da975875b2702b69d3d7adc62abf433eb87cac7f
MmedaraU <54458879+MmedaraU@users.noreply.github.com>
2020-03-26 00:36:00 +0100

    Update Stage1.js

--- Stage1.js
+++ Stage1.js
@@ -1,5 +1,9 @@
-var me = Mmedara;
-var courses = ["HTML","CSS", "JavaScript"];
+let name = 'Mmedara';
+console.log(name);
+
+var courses = ["HTML","CSS","JavaScript"];
+console.log(courses);
+
 function printOdd(200) {
   for (let i=1; i<=200; i++) {
     if(i % 2 !== 0) {
