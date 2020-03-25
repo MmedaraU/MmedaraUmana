edc260fa79967e3521247537fbcadcf2161ea433
MmedaraU <54458879+MmedaraU@users.noreply.github.com>
2020-03-25 13:00:06 +0100

    Update Stage1.js

--- Stage1.js
+++ Stage1.js
@@ -1,6 +1,6 @@
 var me = Mmedara;
 var courses = ["HTML","CSS", "JavaScript"];
-function printOdd(num) {
+function printOdd(200) {
   for (let i=1; i<=200; i++) {
     if(i % 2 !== 0) {
       console.log(i);
