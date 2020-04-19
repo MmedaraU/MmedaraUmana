3daf2c83e3311f3af2eddac02c33cb7fc3f5a535
MmedaraU <54458879+MmedaraU@users.noreply.github.com>
2020-04-19 16:47:06 +0100

    Add files via upload

---
+++ New file2.js
@@ -0,0 +1,26 @@
+function divide(old) {
+    let info = [];
+    
+    for (var i=1; i<=200; i++){
+        if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0){
+       return info.push("yu-gi-oh")
+        }else if (i % 2 == 0 && i % 3 == 0){
+       return info.push("gi-oh")
+   }else if (i % 2 == 0 && i % 5 == 0){
+       return info.push("yu-oh")
+   }else if (i % 2 == 0){
+       return info.push("yu")
+    }else if (i % 3 == 0){
+       return info.push("gi")
+ }else if (i % 5 == 0){
+       return info.push("oh")
+}else{
+    return info.push(i)
+};
+return info
+console.log(info)
+console.log(divide(200))
+}
+divide(200)
+
+ if(i%2==0)
