4de8cdd38cb4b8bb696cf1a0a68da970539425e7
MmedaraU <54458879+MmedaraU@users.noreply.github.com>
2020-04-20 22:44:52 +0100

    Update New file2.js

--- New file2.js
+++ New file2.js
@@ -1,7 +1,7 @@
-function divide(old) {
+function divide(number) {
     let info = [];
     
-    for (var i=1; i<=200; i++){
+    for (var i=1; i<=number; i++){
         if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0){
        return info.push("yu-gi-oh")
         }else if (i % 2 == 0 && i % 3 == 0){
